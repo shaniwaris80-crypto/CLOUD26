35b50b58d557d8371090c3de2405218f8a7dea3a
shaniwaris80-crypto <shaniwaris80@gmail.com>
2026-02-15 12:34:00 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyDR3JDEO_xzfAH9S9aWv2Alrzn0K7s7P9w",
+  authDomain: "cloud26-4ad71.firebaseapp.com",
+  projectId: "cloud26-4ad71",
+  storageBucket: "cloud26-4ad71.firebasestorage.app",
+  messagingSenderId: "214370907801",
+  appId: "1:214370907801:web:a1ead9a8e9f973af09aa08",
+  measurementId: "G-4F2JQH52JP"
+};
